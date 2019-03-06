fb7f8724b73de14a6e1828747e8f0cd34631b39c
Cristiam Diaz <c.diaz@udla.edu.co>
2019-03-06 18:05:15 -0500

    set configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dbconnection } from './db.provider';
+import { Config } from './common/config.provider';
+import { dbconnection } from './common/db.provider';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, dbconnection],
-  exports: [dbconnection]
+  providers: [AppService, Config, dbconnection],
+  exports: [Config, dbconnection]
 })
 export class AppModule {}
